a1fb57e42045f5bd8d8755c8705eeb27536e6914
DylanRhinehart <dylanrhinehart@gmail.com>
2023-01-31 13:19:38 -0800

    commit tree 67d1b168ad8a5e9d2675024504274131d5c8d0c2

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.include?('.') 
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.include?('?')
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.include?('!')
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    word_array = self.split(' ')
+    if word_array.length >= 1 
+     word_array.count
+    else
+      0
+    end
   end
+
 end
